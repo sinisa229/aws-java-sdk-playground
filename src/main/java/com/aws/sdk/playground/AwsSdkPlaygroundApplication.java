306a723f47b9be0815690e8408463705ecaf1f3c
SMihajlovski <sinisha.mihajlovski@esure.com>
2021-10-09 19:22:57 +0100

    Initial code

---
+++ src/main/java/com/aws/sdk/playground/AwsSdkPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.aws.sdk.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSdkPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSdkPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/sdk/playground/AwsSdkPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.sdk.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSdkPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
